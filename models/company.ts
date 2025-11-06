597c6e76cb7ca61bcbbbfde8bef24191c1b3850d
Shyam kumar <83579268+shyam242@users.noreply.github.com>
2025-11-06 23:17:55 +0530

    Update company.ts

--- models/company.ts
+++ models/company.ts
@@ -3,6 +3,8 @@ import mongoose, { Schema, models } from "mongoose";
 const companySchema = new Schema(
   {
     name: { type: String, required: true },
+    processType: { type: String, required: true },
+    processDate: { type: String, required: true },
   },
   { timestamps: true }
 );
